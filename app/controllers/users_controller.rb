12d3c95f14d875b90e0e2947d973ea0a5368410b
daisuke0125 <daisukeno19840125@gmail.com>
2019-07-20 17:37:25 +0900

    フロント実装コード

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  
+  def edit
+  end
+
+  def sign_in
+  end
+
+  def sign_up
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root 'messages#index' #top画面
+  get 'users' => 'users#edit' #アカウント編集画面
+  post 'users' => 'sign_in' #ログイン画面
+  post 'users' => 'sign_up' #新規登録画面
+  post 'groups' => 'new' #グループ作成画面
 end
+  
