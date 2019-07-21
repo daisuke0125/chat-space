123c759fd8305562f65cb9d6f9ed98861859ce00
daisuke0125 <daisukeno19840125@gmail.com>
2019-07-21 12:48:06 +0900

    ルーティングをresoucesを用いて添削
    ・ルーティングをresoucesを用いて添削
    ・プルリクエストの詳細[What]部分を書き換え
    ・チャット画面のキャプチャを添付

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index' #top画面
-  get 'users' => 'users#edit' #アカウント編集画面
-  post 'users' => 'sign_in' #ログイン画面
-  post 'users' => 'sign_up' #新規登録画面
-  post 'groups' => 'new' #グループ作成画面
+  root 'messages#index' 
+  resources :users, only: %i(index edit update)
+  resources :groups, only: %i(new create edit update) do
+    resources :messages, only: %i(index create)
+  end
+
 end
-  
