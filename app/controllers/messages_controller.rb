a07b0b3d6bda89011405e80bff2f820ae4e5a106
daisuke0125 <daisukeno19840125@gmail.com>
2019-07-22 15:27:10 +0900

    投稿機能の実装
    素材のダウンロード
    deviseのインストール
    usersモデルを作成する
    deviseのビューファイルの追加
    サインアップ機能の追加
    ユーザー情報編集機能の追加
    サインアウト/サインイン機能の追加
    フラッシュメッセージの表示機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,4 @@
 class MessagesController < ApplicationController
-
   def index
   end
-
-  def new
-  end
-
-  def login
-  end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index' 
-  resources :users, only: %i(index edit update)
-  resources :groups, only: %i(new create edit update) do
-    resources :messages, only: %i(index create)
+  resources :users, only: [:edit, :update]
   end
-
-end
