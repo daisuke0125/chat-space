d3f52c7db84f34dbe2267fd3969e82b43fb91377
daisuke0125 <daisukeno19840125@gmail.com>
2019-07-26 19:22:54 +0900

    APIの準備
    ・ルーティングの追加設定
    
    ・users_controllerのindexアクションを追加
    
    ・index用のjbuilderを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def index
+    @users = User.new.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html 
+      format.json
+
   def update
     if current_user.update(user_params)
       redirect_to root_path
